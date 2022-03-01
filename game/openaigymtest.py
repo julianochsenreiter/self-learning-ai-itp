76775c133802ec354e4bd84dfc2f9a79231d0362
julianochsenreiter <74651560+julianochsenreiter@users.noreply.github.com>
2022-03-01 11:05:38 +0100

    Update openaigymtest.py
    openai gym beispiele

--- game/openaigymtest.py
+++ game/openaigymtest.py
@@ -1,16 +1,16 @@
-import gym
-env = gym.make('CartPole-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+# import gym
+# env = gym.make('CartPole-v0')
+# for i_episode in range(20):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         print(observation)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+# env.close()
 
 # import gym
 # env = gym.make('CartPole-v0')
@@ -31,3 +31,56 @@ env.close()
 # x = space.sample()
 # assert space.contains(x)
 # assert space.n == 8
+
+import gym
+import numpy as np 
+# 1. Load Environment and Q-table structure
+env = gym.make('FrozenLake8x8-v1')
+Q = np.zeros([env.observation_space.n,env.action_space.n])
+# env.observation.n, env.action_space.n gives number of states and action in env loaded
+# 2. Parameters of Q-learning
+eta = .628
+gma = .9
+epis = 5000
+rev_list = [] # rewards per episode calculate
+# 3. Q-learning Algorithm
+for i in range(epis):
+    # Reset environment
+    s = env.reset()
+    rAll = 0
+    d = False
+    j = 0
+    #The Q-Table learning algorithm
+    while j < 99:
+        env.render()
+        j+=1
+        # Choose action from Q table
+        a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+        #Get new state & reward from environment
+        s1,r,d,_ = env.step(a)
+        #Update Q-Table with new knowledge
+        Q[s,a] = Q[s,a] + eta*(r + gma*np.max(Q[s1,:]) - Q[s,a])
+        rAll += r
+        s = s1
+        if d == True:
+            break
+    rev_list.append(rAll)
+    env.render()
+print("Reward Sum on all episodes " + str(sum(rev_list)/epis))
+print("Final Values Q-Table")
+print(Q)
+
+# Reset environment
+s = env.reset()
+d = False
+# The Q-Table learning algorithm
+while d != True:
+    env.render()
+    # Choose action from Q table
+    a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+    #Get new state & reward from environment
+    s1,r,d,_ = env.step(a)
+    #Update Q-Table with new knowledge
+    Q[s,a] = Q[s,a] + eta*(r + gma*np.max(Q[s1,:]) - Q[s,a])
+    s = s1
+# Code will stop at d == True, and render one state before it
