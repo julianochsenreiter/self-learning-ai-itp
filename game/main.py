81e4d36639db73a11a4cc1bbe080fa8e0746c00c
julianochsenreiter <74651560+julianochsenreiter@users.noreply.github.com>
2022-03-15 11:23:13 +0100

    Update main.py

--- game/main.py
+++ game/main.py
@@ -1,11 +1,12 @@
 import pygame
+from Ship import ship
 from pygame.locals import (
     QUIT,
     KEYDOWN
 )
-
 # init pygame
 pygame.init()
+ship = ship()
 
 width = 800
 height = 600
@@ -15,5 +16,10 @@ screen = pygame.display.set_mode((width, height))
 run = True;
 while run:
     for event in pygame.event.get():
-        if(event.type == QUIT):
+        if event.type == QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                ship.down()
+            elif event.key == pygame.K_UP:
+                ship.up()
