e15610d31020c286ced2d9261d3473b708c7035f
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-03-29 12:37:06 +0200

    env class

---
+++ game/aimenv.py
@@ -0,0 +1,26 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class AIMEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        # TODO
+        pass
+
+    def step(self, action):
+        # TODO
+        pass
+        
+    def reset(self):
+        # TODO
+        pass
+        
+    def render(self, mode='human'):
+        # TODO
+        pass
+
+    def close(self):
+        # TODO
+        pass
