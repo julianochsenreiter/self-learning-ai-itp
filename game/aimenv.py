31c8c494dec35b3ba5aa030c40e44c615308cc31
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-04-11 14:05:53 +0200

    wip env

--- game/aimenv.py
+++ game/aimenv.py
@@ -14,48 +14,22 @@ class AIMEnv(gym.Env):
         pygame.init()
         pygame.font.init()
         
-        key = ""
-        screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("A.I.M.")
-        font = pygame.font.SysFont("Segue UI", 50)
 
-        run = True
-        restart()
-
-        scr = os.path.dirname(__file__)
-        os.chdir(scr)
-
-        highscore = readScore()
-        score = 0
-        
-        while run:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    run = False
-                if event.type == pygame.KEYDOWN: 
-                    if event.key == pygame.K_DOWN:
-                        key = "down"
-                    elif event.key == pygame.K_UP:
-                        key = "up"
-                if event.type == pygame.KEYUP:
-                    key = ""
-            if key == "up":
-                ship.up(20)
-            if key == "down":
-                ship.down(20)
-
-            )  
+        self.score = 0
+        self.obstacles = []
 
     def step(self, action):
         self.screen.fill(BACKGROUND)
         shiprect = ship.draw()
 
-        if len(self.obstacles) < 4 and canAddObstacle(obstacles):
-            obstacles.append(obstacle.Obstacle(screen, spawndist))
-        for o in obstacles:
+        if len(self.obstacles) < 4 and canAddObstacle(self.obstacles):
+            self.obstacles.append(obstacle.Obstacle(self.screen, spawndist))
+        for o in self.obstacles:
             if o.isTouching(shiprect):
                 restart()
-                score = 0
+                
             
             if o.xpos < minpos:
                 obstacles.remove(o)
@@ -64,11 +38,10 @@ class AIMEnv(gym.Env):
                     highscore = score
                     writeScore(score)
             o.draw()
-            o.move(20
+            o.move(20)
         
     def reset(self):
-        # TODO
-        pass
+        score = 0
         
     def render(self, mode='human'):
         # TODO
@@ -78,30 +51,26 @@ class AIMEnv(gym.Env):
         # TODO
         pass
 
-    # utils
-    
-    width = 800
-    height = 600
-    BACKGROUND = (20, 20, 20)
-    spawndist = AIMEnv.getWidth(1+(obstacle.hgap*2))
-    minpos = -100
+# utils
 
-    @staticmethod
-    def getHeight(percent):
-        return height * percent
+width = 800
+height = 600
+BACKGROUND = (20, 20, 20)
+spawndist = AIMEnv.getWidth(1+(obstacle.hgap*2))
+minpos = -100
 
-    @staticmethod
-    def getWidth(percent):
-        return width * percent
+def getHeight(percent):
+    return height * percent
 
-    @staticmethod
-    def obstacleDist():
-        return getWidth(obstacle.hgap)
-    
-    @staticmethod
-    def canAddObstacle(list):
-        for e in list:
-            if isinstance(e, obstacle.Obstacle):
-                if e.xpos > obstacleDist():
-                    return False
-        return True
+def getWidth(percent):
+    return width * percent
+
+def obstacleDist():
+    return getWidth(obstacle.hgap)
+
+def canAddObstacle(list):
+    for e in list:
+        if isinstance(e, obstacle.Obstacle):
+            if e.xpos > obstacleDist():
+                return False
+    return True
