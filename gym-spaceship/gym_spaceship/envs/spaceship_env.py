7a35240beeb882c756a9127f393361b70aac6ba9
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-05-03 08:08:21 +0200

    all classes in one file for now

--- gym-spaceship/gym_spaceship/envs/spaceship_env.py
+++ gym-spaceship/gym_spaceship/envs/spaceship_env.py
@@ -1,11 +1,14 @@
+from typing import Tuple
+import random
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
 
-from ship import Ship
-import obstacle
 import pygame
+from pygame.sprite import Sprite
+from pygame.surface import Surface
 
 # https://www.gymlibrary.ml/
 
@@ -19,8 +22,8 @@ class SpaceshipEnv(gym.Env):
         self.observation_space = spaces.Dict(
             {
                 "ship": spaces.Discrete(height),
-                "top": spaces.Discrete(height),
-                "bottom": spaces.Discrete(height)
+                "top": spaces.Discrete(spawndist+abs(minpos)+2),
+                "bottom": spaces.Discrete(spawndist+abs(minpos)+2, start=minpos)
             }
         )
 
@@ -30,7 +33,7 @@ class SpaceshipEnv(gym.Env):
 
         # init gameplay
         self.obstacles = []
-        self.ship = ship.Ship()
+        self.ship = Ship()
         self.score = 0
 
     def step(self, action):
@@ -44,10 +47,10 @@ class SpaceshipEnv(gym.Env):
 
         if len(self.obstacles) < 4 and canAddObstacle(self.obstacles):
             if self.seed != None and isinstance(self.seed):
-                self.obstacles.append(obstacle.Obstacle(spawndist, self.seed))
+                self.obstacles.append(Obstacle(spawndist, self.seed))
                 
             else:
-                self.obstacles.append(obstacle.Obstacle(spawndist))
+                self.obstacles.append(Obstacle(spawndist))
         
         for o in self.obstacles:
             if o.isTouching(self.ship.position):
@@ -119,6 +122,7 @@ class SpaceshipEnv(gym.Env):
             }
 
         o = self.obstacles[0]
+        print(f"{o.top.rect.left=} {o.bottom.rect.left}")
         return {
             "ship": self.ship.ypos, 
             "top": o.top.rect.left,
@@ -129,13 +133,125 @@ class SpaceshipEnv(gym.Env):
         return {
             "score": self.score
         }
+class Ship:
+    # speed = 20
+    # vertspeed = 200
+    # xpos = 10
+    # ypos = 50
+    # display = pygame.display.set_mode((800,600)) 
 
+    @property
+    def position(self):
+        return (self.xpos, self.ypos)
+
+    def __init__(self):
+        self.speed = 20
+        self.vertspeed = 200
+        self.xpos = 100
+        self.ypos = 50
+        self.display = pygame.display.set_mode((800,600))
+        # inpos = Sprite()
+    
+    def up(self, speed: int):
+        self.ypos -= speed
+        if self.ypos < 10:
+            self.ypos = 10
+        # print(self.ypos)
+
+    def down(self, speed: int):
+        self.ypos += speed
+        if self.ypos > 590:
+            self.ypos = 590
+        # print(self.ypos)
+
+    def draw(self) -> pygame.Rect:
+        return pygame.draw.polygon(self.display, color=(255,80,80), points=[(self.xpos, self.ypos+10), (self.xpos+30,self.ypos), (self.xpos,self.ypos-10)])
+class Obstacle:
+    @property
+    def xpos(self):
+        return self.top.rect.left
+
+    def __init__(self, x: int, seed : int = None):
+        from .spaceship_env import (getWidth, getHeight)
+        # generate distance
+        if seed != None:
+            random.seed(seed)
+        topdist = random.random()
+
+        buffer = minheight + hgap
+        max = 1 - buffer
+        if topdist > max:
+            topdist = max
+        bottomdist = 1 - topdist - hgap
+
+        #top
+        self.top = Sprite()
+        self.top.surf = Surface((getWidth(width), getHeight(topdist)))
+        self.top.surf.fill(OBSTC)
+        self.top.rect = self.top.surf.get_rect()
+        self.top.rect.topleft = (x, 0)
+
+        #bottom
+        self.bottom = Sprite()
+        self.bottom.surf = Surface((getWidth(width), getHeight(bottomdist)))
+        self.bottom.surf.fill(OBSTC)
+        self.bottom.rect = self.bottom.surf.get_rect()
+        self.bottom.rect.topleft = (x, getHeight(1-bottomdist))
+
+
+        # print(f"Created obstacle at {x} ({self.xpos})")
+
+    def draw(self, surf):
+        """draw on surface"""
+        surf.blit(self.top.surf,self.top.rect)
+        surf.blit(self.bottom.surf, self.bottom.rect)
+    
+    def move(self, dist: float):
+        """Move left a certain distance"""
+        self.top.rect.move_ip(-dist, 0)
+        self.bottom.rect.move_ip(-dist, 0)
+        # print(f"Moving by {dist} ({self.xpos})")
+    
+    def isTouching(self, r : Tuple[float, float] | pygame.Rect ):
+        if isinstance(r, pygame.Rect):
+            # check top
+            if self.top.rect.colliderect(r):
+                return True
+            
+            # check bottom
+            if self.bottom.rect.colliderect(r):
+                return True
+        elif isinstance(r, tuple):
+            if self.top.rect.collidepoint(r):
+                return True
+            
+            if self.bottom.rect.collidepoint(r):
+                return True
+        else:
+            raise ValueError
+        
+        return False
 # utils
 
 width = 800
 height = 600
 BACKGROUND = (20, 20, 20)
 minpos = -100
+
+# All values are % of the screen
+# the gap between the top and the bottom pipe
+vgap = 0.15
+
+# the gap between the sets of pipes
+hgap = 0.25
+
+# the width of the pipes
+width = 0.1
+
+# minimum height of the pipes
+minheight = 0.1
+
+OBSTC = pygame.Color(225, 255, 225)
 
 def getHeight(percent):
     return height * percent
@@ -144,13 +260,13 @@ def getWidth(percent):
     return width * percent
 
 def obstacleDist():
-    return getWidth(obstacle.hgap)
+    return getWidth(hgap)
 
 def canAddObstacle(list):
     for e in list:
-        if isinstance(e, obstacle.Obstacle):
+        if isinstance(e, Obstacle):
             if e.xpos > obstacleDist():
                 return False
     return True
 
-spawndist = getWidth(1+(obstacle.hgap*2))
+spawndist = getWidth(1+(hgap*2))
