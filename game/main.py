7f2c868d8a3fb0f503d6ef96854adc7b6a189121
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-05-03 09:05:16 +0200

    basic ai

--- game/main.py
+++ game/main.py
@@ -3,6 +3,18 @@ import gym
 
 def main():
     env = gym.make("gym_spaceship/Spaceship-v0")
+    env.action_space.seed(42)
+
+    observation, info = env.reset(seed=42, return_info=True)
+
+    for _ in range(1000):
+        observation, reward, done, info = env.step(env.action_space.sample())
+        env.render()
+
+        if done:
+            observation, info = env.reset(return_info=True)
+
+    env.close()
 
 if __name__ == "__main__":
     main()
